f03a643b2a3b645fde8d7d3311ad5c7686c86981
mk222xa <mk222xa@student.lnu.se>
2013-12-03 00:15:29 +0100

    Constructor Message.js
    Minor HTML fixes as well.

--- 2-labbymezzage/js/Message.js
+++ 2-labbymezzage/js/Message.js
@@ -1,1 +1,33 @@
 "use strict";
+
+function Message(message, date) {
+    
+    this.getText = function () {
+        return message;
+    }
+
+    this.setText = function (_text) {
+        message = text;
+    }
+
+    this.getDate = function () {
+        return date;
+    }
+
+    this.setDate = function (_date) {
+        date = Date();
+    }
+
+    Message.prototype.toString = function () {
+        return this.getText() + " (" + this.getDate() + ")";
+    };
+
+    Message.prototype.getHTMLText = function () {
+        return this.getText().replace(/[\n\r]/g, "<br />");
+    };
+
+    Message.prototype.getDateText = function () {
+        var messTime = this.getDate();
+        return messTime.getHours() + ":" +messTime.getMinutes() + ":" +messTime.getSeconds();
+    }
+};
