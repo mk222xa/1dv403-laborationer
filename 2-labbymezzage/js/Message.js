3b2a504e8015b81b3c07b555fbb7c96b94aaed9e
mk222xa <mk222xa@student.lnu.se>
2013-12-03 23:39:14 +0100

    Constructor fix
    Working as intended now

--- 2-labbymezzage/js/Message.js
+++ 2-labbymezzage/js/Message.js
@@ -1,13 +1,13 @@
 "use strict";
 
 function Message(message, date) {
-    
+
     this.getText = function () {
         return message;
     };
 
     this.setText = function (_text) {
-        message = text;
+        message = _text;
     };
 
     this.getDate = function () {
@@ -15,19 +15,19 @@ function Message(message, date) {
     };
 
     this.setDate = function (_date) {
-        date = messageDate();
+        date = _date;
     };
-
-    Message.prototype.toString = function () {
-        return this.getText() + " (" + this.getDate() + ")";
-    };
-
-    Message.prototype.getHTMLText = function () {
-        return this.getText().replace(/[\n\r]/g, "<br />");
-    };
-
-    Message.prototype.getDateText = function () {
-        return this.getDate().toLocaleString();
-    };
+}
+Message.prototype.toString = function () {
+    return this.getText() + " (" + this.getDate() + ")";
 };
+
+Message.prototype.getHTMLText = function () {
+    return this.getText().replace(/[\n\r]/g, "<br />");
+};
+
+Message.prototype.getDateText = function () {
+    return "Inlägget skapades: " + this.getDate().toLocaleString();
+};
+
 
--- 2-labbymezzage/js/MessageBoard.js
+++ 2-labbymezzage/js/MessageBoard.js
@@ -1,2 +1,12 @@
 "use strict";
 
+
+    var testMess = new Message("Test", new Date());
+
+    alert(testMess);
+    alert(testMess.getText());
+    testMess.setText("Ny test text");
+    alert(testMess);
+
+
+window.onload = MessageBoard.init;
