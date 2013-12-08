dbf1eb473d3eb26119c474b9d78f74ca33fe4bda
mk222xa <mk222xa@student.lnu.se>
2013-12-08 11:48:25 +0100

    LabbyMezzage late push
    copied from laptop offline

--- 2-labbymezzage/js/Message.js
+++ 2-labbymezzage/js/Message.js
@@ -1,19 +1,19 @@
 "use strict";
 
 function Message(message, date) {
-
+    
     this.getText = function () {
         return message;
     };
-
+    
     this.setText = function (_text) {
         message = _text;
     };
-
+    
     this.getDate = function () {
         return date;
     };
-
+    
     this.setDate = function (_date) {
         date = _date;
     };
@@ -25,9 +25,27 @@ Message.prototype.toString = function () {
 Message.prototype.getHTMLText = function () {
     return this.getText().replace(/[\n\r]/g, "<br />");
 };
-
+//Used for the Alert window when clicking the info button
 Message.prototype.getDateText = function () {
-    return "Inl‰gget skapades: " + this.getDate().toLocaleString();
+    return "Inl√§gget skapades: " + this.getDate().toLocaleString();
 };
 
-
+//Used for timestamp on messages on screen
+Message.prototype.getTimeStamp = function() {
+    var str = "";
+    
+    var currentTime = this.getDate()
+    var hours = currentTime.getHours()
+    var minutes = currentTime.getMinutes()
+    var seconds = currentTime.getSeconds()
+    
+    if (minutes < 10) {
+        minutes = "0" + minutes
+    }
+    if (seconds < 10) {
+        seconds = "0" + seconds
+    }
+    str += hours + ":" + minutes + ":" + seconds;
+    
+    return str;
+};
--- 2-labbymezzage/js/MessageBoard.js
+++ 2-labbymezzage/js/MessageBoard.js
@@ -1,12 +1,119 @@
 "use strict";
 
+var MessageBoard = {
 
-    var testMess = new Message("Test", new Date());
+    messages: [],
 
-    alert(testMess);
-    alert(testMess.getText());
-    testMess.setText("Ny test text");
-    alert(testMess);
+    init: function () {
+        //get the message when clicking the button and put it in the array and render on screen
+        document.getElementById("post").onclick = function getMessage(e) {
+            e.preventDefault();
+            var text = document.getElementById("messageInput").value;
+            document.getElementById("messageInput").value = "";
+            var mess = new Message(text, new Date());
+            MessageBoard.messages.push(mess);
+            MessageBoard.renderMessages();
+        };
 
+        //get the message and put in the array and render on screen
+        document.getElementById("messageInput").onkeypress = function (e) {
+            var code;
+            if (!e) {
+                e = window.event;
+            }
+            if (e.keyCode) {
+                code = e.keyCode;
+            } else if (e.which) {
+                code = e.which;
+            }
+
+            if (e.shiftKey && code === 13) {
+                document.textarea.value += "\n";
+                return false;
+            } else if (code === 13) {
+                var text = document.getElementById("messageInput").value;
+                document.getElementById("messageInput").value = "";
+                var mess = new Message(text, new Date());
+                MessageBoard.messages.push(mess);
+                MessageBoard.renderMessages();
+                return false;
+            }
+        };
+    },
+
+    //removes the messages and prints them all from array to screen
+    renderMessages: function () {
+        
+        document.getElementById("messageboard").innerHTML = "";
+        
+        for (var i = 0; i < MessageBoard.messages.length; ++i) {
+            MessageBoard.renderMessage(i);
+        }
+        // Counter for messages
+        var counter = document.getElementById("counter");
+        var number = (MessageBoard.messages.length);
+        counter.innerHTML = "Antal meddelande : " +number;
+    },
+
+    // Creates HTML tags for the new message
+    renderMessage: function (messageID) {        
+        var text = document.createElement("div");
+        var p = document.createElement("p");
+        text.className = "messageFromUser";
+        p.className = "input";
+
+        // Element for the icons
+        var msgIcon = document.createElement("span");
+        msgIcon.className = "icons";
+
+        //Date and time icon
+        var time = document.createElement("a");
+        var imgDateTime = document.createElement("img");
+        imgDateTime.className = "datetime";
+        imgDateTime.setAttribute("src", "pics/clock.png");
+        imgDateTime.alt = "ShowDateTime";
+
+        //Function for showing date and time when the message was created when clicked
+        imgDateTime.onclick = function () {
+            alert(MessageBoard.messages[messageID].getDateText());
+        };
+
+        var removePost = document.createElement("a");
+        var imgClose = document.createElement("img");
+        imgClose.className = "deletebutton";
+        imgClose.setAttribute("src", "pics/delete.png");
+        imgClose.alt = "Close";
+
+        //Delete function bound to the delete.png image
+        imgClose.onclick = function () {
+            var result = confirm("Vill du ta bort meddelandet?");
+            if (result === true) {
+                MessageBoard.removeMessage(messageID);
+            }
+        };
+
+        msgIcon.appendChild(time);
+        time.appendChild(imgDateTime);
+        msgIcon.appendChild(removePost);
+        removePost.appendChild(imgClose);
+
+        p.innerHTML = MessageBoard.messages[messageID].getHTMLText();
+
+        var messageTime = document.createElement("p");
+        messageTime.className = "time";        
+        messageTime.innerHTML = MessageBoard.messages[messageID].getTimeStamp();
+
+        text.appendChild(msgIcon);
+        text.appendChild(p);
+        text.appendChild(messageTime);
+
+        document.getElementById("messageboard").appendChild(text);
+    },
+
+    removeMessage: function (deleteMess) {
+        MessageBoard.messages.splice(deleteMess, 1);
+        MessageBoard.renderMessages();
+    }
+};
 
 window.onload = MessageBoard.init;
