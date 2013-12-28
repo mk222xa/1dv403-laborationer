03e192ecb4890d4094b44d37cb9caef65cedbc35
mk222xa <mk222xa@student.lnu.se>
2013-12-28 16:39:28 +0100

    minor fix
    added ID and variables

---
+++ 4-forms/script/script.js
@@ -0,0 +1,13 @@
+"use strict"
+
+window.onload = function() {
+    
+    var doc = document;
+    var name = doc.getElementById("name");
+    var surname = doc.getElementById("surname");
+    var zipcode = doc.getElementById("zipcode");
+    var email = doc.getElementById("email");
+    var priceModel = doc.getElementById("priceModel");
+    var buttonSend = doc.getElementById("buttonSend");
+    
+};
