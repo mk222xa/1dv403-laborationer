7a0eed89c0f5d74edeecb65bad4b4256a58a003b
mk222xa <mk222xa@student.lnu.se>
2014-01-05 05:33:49 +0100

    validation function is awesome
    Need to fix email regexp

--- 4-forms/script/script.js
+++ 4-forms/script/script.js
@@ -1,7 +1,8 @@
 "use strict"
 
-window.onload = function() {
-    
+window.onload = function () {
+
+    //Variables for the input fields
     var doc = document;
     var name = doc.getElementById("name");
     var surname = doc.getElementById("surname");
@@ -9,5 +10,45 @@ window.onload = function() {
     var email = doc.getElementById("email");
     var priceModel = doc.getElementById("priceModel");
     var buttonSend = doc.getElementById("buttonSend");
+
+
+    //Variables for regexp 
+    var nameRegexp = /^[a-zA-Z-åäöÅÄÖ]*[a-zA-Z-åäöÅÄÖ]+[a-zA-Z-åäöÅÄÖ]*$/;
+    var zipcodeRegexp = /^(SE)*\s*\d{3}[\ \-]*\d{2}$/;
+    var emailRegexp = /^[a-zA-Z0-9]+@[a-zA-Z0-9\.]+[\.]{1}[a-zA-Z]{2,4}$/;
+
+    //Variables for messages
+
+    var nameError = "Ej korrekt ifylld";
+    var zipError = "Ej korrekt format på postnummer";
+    var emailError = "Ej korrekt format på emailadress";
     
+    //Checking the fields onblur
+    name.onblur = function(){validate(name, nameRegexp, nameError);};
+    surname.onblur = function(){validate(surname, nameRegexp, nameError);};
+    zipcode.onblur = function(){validate(zipcode, zipcodeRegexp, zipError);};
+    email.onblur = function(){validate(email, emailRegexp, emailError);};
+    
+    //function for validating the fields
+    function validate(input, regexp, message) {
+        if (regexp.test(input.value)) {
+            if (doc.getElementById(input.getAttribute("id") + "message") !== null) {
+                var invalid = doc.getElementById(input.getAttribute("id") + "message");
+                invalid.parentNode.removeChild(invalid);
+            }
+            return true;
+        } else {
+            if (doc.getElementById(input.getAttribute("id") + "message") === null) {
+
+                var invalid = doc.createElement("p");
+                invalid.setAttribute("id", input.getAttribute("id") + "message");
+                invalid.appendChild(doc.createTextNode(message));
+                input.parentNode.insertBefore(invalid, input.nextSibling);
+            }
+            return false;
+        }
+    }
+
+
+
 };
