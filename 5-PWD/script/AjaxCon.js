ab48d4f5fa9f0a29ee381ea3d4c7694ef1638520
mk222xa <mk222xa@student.lnu.se>
2014-03-24 04:35:49 +0100

    PWD
    Omgjort för nivå 3

---
+++ 5-PWD/script/AjaxCon.js
@@ -0,0 +1,44 @@
+"use strict";
+
+function AjaxCon(url, callback) {
+    var doc = document;
+
+    var READY_STATE_UNINITIALIZED = 0;
+    var READY_STATE_OPENED = 1;
+    var READY_STATE_SENT = 2;
+    var READY_STATE_LOADING = 3;
+    var READY_STATE_COMPLETE = 4;
+
+    var xhr = this.getXHR();
+
+    xhr.onreadystatechange = function () {
+
+        if (xhr.readyState === READY_STATE_COMPLETE) {
+            if (xhr.readyState === 4 && xhr.status === 200 || xhr.status === 304) {
+                callback(xhr.responseText);     
+
+            } else {
+                alert("Läsfel, status:" + xhr.status);
+            }
+        }
+    };
+
+    xhr.open("get", url, true);
+    xhr.send(null);
+
+}
+
+AjaxCon.prototype.getXHR = function () {
+    var xhr = null;
+
+    try {
+        xhr = new XMLHttpRequest();
+    } catch (error) {
+        try {
+            xhr = new ActiveXObject("Msxml2.XMLHTTP");
+        } catch (error) {
+            throw new Error("Ajax stöds inte av din webläsare, var vänlig uppgradera till senaste versionen");
+        }
+    }
+    return xhr;
+};
